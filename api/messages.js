20556553b6d0780779a02e600c67232358bf1e6d
Shakir Salmani <salmanim281@gmail.com>
2026-05-24 00:15:07 +0530

    Fix package json

---
+++ api/messages.js
@@ -0,0 +1,58 @@
+const {
+    connectDB
+} = require(
+    '../src/utils/chatLogger'
+);
+
+module.exports =
+async function handler(
+    req,
+    res
+) {
+
+    try {
+
+        const db =
+            await connectDB();
+
+        const collection =
+            db.collection(
+                'messages'
+            );
+
+        // =========================
+        // GET ALL CHATS
+        // =========================
+
+        const messages =
+            await collection
+
+                .find({})
+
+                .sort({
+                    createdAt: 1
+                })
+
+                .toArray();
+
+        return res.status(200).json({
+
+            success: true,
+
+            messages
+        });
+
+    } catch (error) {
+
+        console.log(
+            '❌ FETCH MESSAGES ERROR'
+        );
+
+        console.log(error);
+
+        return res.status(500).json({
+
+            success: false
+        });
+    }
+};
